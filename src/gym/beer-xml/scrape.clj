b94f6a75b5afcda3c96156757e245a0920dda8d0
Nick Nichols <nick.nichols@rate.com>
2019-09-09 09:47:22 -0500

    TO-DO BeerXML

---
+++ src/gym/beer-xml/scrape.clj
@@ -0,0 +1,9 @@
+(ns gym.beer-xml.scrape
+  (:gen-class))
+
+(def todo
+  {1 "Download recipe listingh @ https://www.kaggle.com/jtrofe/beer-recipes"
+   2 "Convert URL links to the form https://www.brewersfriend.com/homebrew/recipe/downloadbeerxml/5920"
+   3 "Download beerXML and parse to brew-bot format"
+   4 "Make brew-bot BeerXML compliant"
+   5 "Update brew-bot to generate by styles using the above data"})
