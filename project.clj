2f9fd76e2375832a5a5ed6c3e089279015b064fc
nnichols <nichols1991@gmail.com>
2021-09-16 09:20:32 -0500

    Fix license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject gym "0.0.0"
   :description "A bunch of code used for heavy lifting and general training"
   :url "https://github.com/nnichols/gym"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "MIT"
+            :url  "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [cheshire "5.10.0"]
                  [clj-http "3.11.0"]
